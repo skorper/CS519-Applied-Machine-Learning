9501798d3858f56f7d25af3866a7ef0403df0402
Stepheny <s_perez@nmsu.edu>
2018-09-17 12:37:08 -0600

    Fixed errors. Still is not converging

--- proj1/perceptron.py
+++ proj1/perceptron.py
@@ -25,33 +25,39 @@
 
 #when the data is not linearly seperable the error may go up and down and it will never converge
 
+import numpy as np
+
 class Perceptron(object):
 	# instance variables: eta (input by user to control how fast toconverge)
 	# w (0...m)
 	# num of iterations (from user)
 	# error (array) implicit
 
-	def _init(self, _eta=0.1, _iter=10):
+	def __init__(self, _eta=0.1, _iter=10):
 		self.eta = _eta
 		self.iter = _iter
 
 	def learn(self, x, y):
+		#define 'm'
+		self.m = x.shape[1]
 		#generate m random w's
-		generator = np,random.RandomState(_, _, _)
-		self.w = generator.normal(_, _, m + 1)
+		generator = np.random.RandomState(1)
+		self.w = generator.normal(0, 0.01, self.m + 1)
+		self.error = []
+
 		for i in range (self.iter):
-			error[i] = 0
-			for xi yi in zip(x, y):
-				y_hat = predict(xi) #w transpose x (using numpy dot(w, xi)) will eventually replace with prediction function
-				error[i] = error[i] + np.where(y_i == y_hat, 0, 1) #after each prediction, count how many are wrong
-				for j in range (m):
-					self.w[j] = self.w[j] + self.eta * (yi - yhat) * xi[j]
+			self.error.append(0)
+			for xi, yi in zip(x, y):
+				y_hat = self.predict(xi) #w transpose x (using numpy dot(w, xi)) will eventually replace with prediction function
+				self.error[i] = self.error[i] + np.where(yi == y_hat, 0, 1) #after each prediction, count how many are wrong
+				for j in range (self.m):
+					self.w[j] = self.w[j] + self.eta * (yi - y_hat) * xi[j]
 
-				self.w[0] = ...
-			if error[i] == 0:  #probably need a threshold
-				#then terminate
-
+				self.w[0] = self.eta * (yi - y_hat) #TODO fix this value
+				if self.error[i] == 0:  #probably need a threshold then terminate
+					break
+			print (self.error)
 	def predict(self, x):
-		input = np.dot(w, x)
+		input = np.dot(self.w[1:], x)
 		v = np.where(input > 0, 1, -1) #where function
 		return v #predicted value
--- proj1/use.py
+++ proj1/use.py
@@ -1,12 +1,16 @@
-df = pd.readcsv(irisdataset, header=none)
+import pandas as pd
+import numpy as np
+from perceptron import Perceptron
+
+df = pd.read_csv('iris.data', header=None)
 
 # right now our perceptron can only do binary class labels (and iris has 3 labels)
 
-x = df.iloc(0:100, [0,2]).values # try to plot these values (first two dimensions) to test convergence and if data is linearly separable
-y = df.iloc[0:100,4].values
-y = np.where(y == "iris-setosa", 1, -1)
+x = df.iloc[0:100, [0,2]].values # try to plot these values (first two dimensions) to test convergence and if data is linearly separable
+y = df.iloc[0:100, 4].values
+y = np.where(y == "Iris-setosa", 1, -1) # todo change this back to lower case
 
-model = Perceptron(eta=0.1, iter=10)
+model = Perceptron(_eta=0.1, _iter=10)
 model.learn(x, y)
 #print error in different iterations
 
