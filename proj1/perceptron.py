1072181b3e79b2b983b9f34d3428b83ae5836651
Stepheny <s_perez@nmsu.edu>
2018-09-19 23:45:44 -0600

    updated models

--- proj1/perceptron.py
+++ proj1/perceptron.py
@@ -1,29 +1,36 @@
-# ML 8.27
+# From notes:
+# class Perceptron(object):
+# 	# instance variables: eta (input by user to control how fast toconverge)
+# 	# w (0...m)
+# 	# num of iterations (from user)
+# 	# error (array) implicit
 
-# 1. Random number generator from numpy
-#
-# numpy random uniform (min/max?)
-# numpy random normal (gaussian?) (standard deviation, scale, size)
+# 	def _init(self, _eta=0.1, _iter=10):
+# 		self.eta = _eta
+# 		self.iter = _iter
 
-# 2. Zip
+# 	def learn(self, x, y):
+# 		#generate m random w's
+# 		generator = np,random.RandomState(_, _, _)
+# 		self.w = generator.normal(_, _, m + 1)
+# 		for i in range (self.iter):
+# 			error[i] = 0
+# 			for xi yi in zip(x, y):
+# 				y_hat = predict(xi) #w transpose x (using numpy dot(w, xi)) will eventually replace with prediction function
+# 				error[i] = error[i] + np.where(y_i == y_hat, 0, 1) #after each prediction, count how many are wrong
+# 				for j in range (m):
+# 					self.w[j] = self.w[j] + self.eta * (yi - yhat) * xi[j]
 
-# for x in zip(xlist)
-# for x_i, y_i in zip(x y)
+# 				self.w[0] = ...
+# 			if error[i] == 0:  #probably need a threshold
+# 				#then terminate
 
-# Dot (from numpy)
+# 	def predict(self, x):
+# 		input = np.dot(w, x)
+# 		v = np.where(input > 0, 1, -1) #where function
+# 		return v #predicted value
 
-#just does dot product
-
-# numpy.dot(x, y) // x T y 
-# can use something else, but recommends using dot because it's faster
-
-# 4. Numpy where()
-
-# 5. Paandas package read_csv [data frame data structure] iloc and values
-
-# Perceptron
-
-#when the data is not linearly seperable the error may go up and down and it will never converge
+# Implementation: 
 
 import numpy as np
 
@@ -38,8 +45,8 @@ class Perceptron(object):
 		self.iter = _iter
 
 	def learn(self, x, y):
-		#define 'm'
-		self.m = x.shape[1]
+		#define 'm' (length of data attrs)
+		self.m = x[1].size
 		#generate m random w's
 		generator = np.random.RandomState(1)
 		self.w = generator.normal(0, 0.01, self.m + 1)
@@ -48,16 +55,19 @@ class Perceptron(object):
 		for i in range (self.iter):
 			self.error.append(0)
 			for xi, yi in zip(x, y):
-				y_hat = self.predict(xi) #w transpose x (using numpy dot(w, xi)) will eventually replace with prediction function
-				self.error[i] = self.error[i] + np.where(yi == y_hat, 0, 1) #after each prediction, count how many are wrong
-				for j in range (self.m):
-					self.w[j] = self.w[j] + self.eta * (yi - y_hat) * xi[j]
+				y_hat = self.predict(xi) #w transpose x (using numpy dot(w, xi))
+				self.w[1:] += self.eta * (yi - y_hat) * xi
+				
+				# for j in range (self.m):
+				# 	self.w[j] += ( self.eta * (yi - y_hat) )* xi
 
-				self.w[0] = self.eta * (yi - y_hat) #TODO fix this value
-				if self.error[i] == 0:  #probably need a threshold then terminate
-					break
-			print (self.error)
+				self.error[-1] += np.where(yi == y_hat, 0, 1) #after each prediction, count how many are wrong
+				self.w[0] = self.eta * (yi - y_hat)
+			if self.error[-1] == 0.0:  #probably need a threshold then terminate. For now use 0
+				break
+			print (self.error[-1])
+
 	def predict(self, x):
-		input = np.dot(self.w[1:], x)
+		input = np.dot(self.w[1:], x) + self.w[0]
 		v = np.where(input > 0, 1, -1) #where function
 		return v #predicted value
---
+++ proj1/sgd.py
@@ -0,0 +1,62 @@
+# From notes;
+#
+# gen = np.RangomGenerator
+# r = gen.permutation(n)
+# x[r] = new vector which is shuffled
+# #y[r] needs to match x[r]
+# for i in range (iter):
+# 	r = shuffle
+# 	x = x[r]
+# 	y = y[r]
+# 	for xi , target in zip(x, y):
+# 			# update weights
+# 			zi = np.dot(xi, w[1]) + self.w[0]
+# 			error = target - zi
+# 			self.w[1:] = self.eta * xi.dot(error)
+# 			self.w[0] = self.eta * error
+# 	cost += 0.5 * error ** 2 
+
+# Implementation: (similar to Adaline)
+
+import numpy as np
+
+class Sgd(object):
+
+	def __init__(self, _eta=0.1, _iter=10):
+		self.eta = _eta
+		self.iter = _iter
+
+	def learn(self, x, y):
+		# s1: initialize w using random gen
+		# this is the same as the perceptron class
+		#define 'm' (length of data attrs)
+		self.m = x[1].size
+		#generate m random w's
+		generator = np.random.RandomState(1)
+		self.w = generator.normal(0, 0.01, self.m + 1)
+		self.cost_ = []
+		for i in range(self.iter):
+			# shuffle training instances for more
+			# stochastic effect
+			r = generator.permutation(len(y))
+			x = x[r]
+			y = y[r]
+			cost = []
+			for xi, target in zip(x, y):
+				zi = np.dot(xi, self.w[1:]) + self.w[0]
+				error = target - zi
+				self.w[1:] += self.eta * xi.dot(error)
+				self.w[0] += self.eta * error
+				cost.append(0.5 * error**2)
+			# average the costs from the training instances 
+			# in this iteration
+			average_cost = sum(cost) / len(y)
+			self.cost_.append(average_cost)
+			print (sum(cost))
+		return self
+
+	# same as perceptron prediction
+	def predict(self, x):
+		input = np.dot(self.w[1:], x) + self.w[0]
+		v = np.where(input > 0, 1, -1) #where function
+		return v #predicted value
