30956b8086f759f3a5364ccd06de9b83df58da73
Stepheny <s_perez@nmsu.edu>
2018-09-20 18:09:00 -0600

    changed adaline data columns

--- proj1/main.py
+++ proj1/main.py
@@ -23,6 +23,7 @@ dataset_columns = len(df.columns)
 true_class = df.iloc[0, dataset_columns - 1] #just pick the first one for binary classification
 
 # Load data (all columns)
+# NOTE: This is different for Adaline. Weird behavior on all columns, so only using two (see report)
 x = df.iloc[0:dataset_size, 0:dataset_columns - 1].values # try to plot these values (first two dimensions) to test convergence and if data is linearly separable
 y = df.iloc[0:dataset_size, dataset_columns - 1].values
 y = np.where(y == true_class, 1, -1) 
@@ -41,11 +42,15 @@ if sys.argv[1] == 'perceptron':
     model = Perceptron(_eta=0.01, _iter=20)
     x_std = x #running Perceptron on x_std gives weird results, so use original x
 elif sys.argv[1] == 'adaline':
+    x = df.iloc[0:dataset_size, [0,dataset_columns - 2]].values
+    x_std = np.copy(x)
+    x_std[:, 0] = (x[:, 0] - x[:, 0].mean()) / x[:, 0].std()
+    x_std[:, 1] = (x[:, 1] - x[:, 1].mean()) / x[:, 1].std()
     model = Adaline(_eta=0.01, _iter=20)
 elif sys.argv[1] == 'sgd':
     model = Sgd(_eta=0.01, _iter=20)
 elif sys.argv[1] == 'ovr':
-    model = Ovr(df.iloc[0:dataset_size, dataset_columns - 1].values, _eta=0.001, _iter=20)
+    model = Ovr(df.iloc[0:dataset_size, dataset_columns - 1].values, _eta=0.01, _iter=20)
 else:
     print ("Incorrect algorithm. Options are perceptron, adaline, or sgd. ")
     exit()
@@ -70,6 +75,8 @@ if sys.argv[1] == 'ovr':
         if prediction == y_tst[i]:
             correct += 1
 else:
+    if sys.argv[1] == 'adaline':
+        x_tst = df.iloc[0:dataset_size, [0,dataset_columns - 2]].values
     for i in range(len(x_tst)):
         prediction = model.predict(x_tst[i])
         if (prediction == 1 and y_tst[i] == true_class) or (prediction == -1 and y_tst[i] != true_class):
